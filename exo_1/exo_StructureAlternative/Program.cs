12cb25837ab02256f5653ffdbfeffa0f60ba6664
biscueil aramis <biscueil@gmail.com>
2018-11-22 08:10:50 +0100

    version 1

---
+++ exo_1/exo_StructureAlternative/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exo_StructureAlternative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez saisir un nombre A:");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Veuillez saisir le nombre B:");
+            int b = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
